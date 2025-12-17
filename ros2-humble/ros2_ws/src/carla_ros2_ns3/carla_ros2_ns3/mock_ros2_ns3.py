9f2b7f6161e89e66e4e434f4c192afc65015bb24
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2025-12-17 15:10:57 +0100

    suppressions des global si possible

--- ros2-humble/ros2_ws/src/carla_ros2_ns3/carla_ros2_ns3/mock_ros2_ns3.py
+++ ros2-humble/ros2_ws/src/carla_ros2_ns3/carla_ros2_ns3/mock_ros2_ns3.py
@@ -9,17 +9,6 @@ import threading
 import rclpy
 
 import carla_ros2_ns3.const as cst
-from carla_ros2_ns3.const import (
-    MTU,
-    vehicles,
-    number_message_sent,
-    number_message_received,
-    stop_state,
-    error_state,
-    comunication_nodes_thread,
-    position_listener_thread,
-    listen_tap_devices_thread
-)
 from carla_ros2_ns3.lib.mock_sim import (
     init_mock
 )
@@ -67,7 +56,7 @@ def control_node_listener(socket_tap0):
     while rclpy.ok():
         try:
 
-            packet = socket_tap0.recv(MTU)
+            packet = socket_tap0.recv(cst.MTU)
 
             # Récupérer l'adresse IP de destination du paquet
             dest_ip = packet[30:34]
@@ -137,63 +126,59 @@ def control_node_listener(socket_tap0):
 
 def launch_simulation(sockets, control_socket):
     """Lance la simulation."""
-    global position_listener_thread
-    global comunication_nodes_thread
-    global listen_tap_devices_thread
 
     interval = 1
 
     # Lancer l'écoute périodique des positions dans un thread
     inflog("Launching  periodic_position_sender")
-    position_listener_thread = threading.Thread(
+    cst.position_listener_thread = threading.Thread(
         target=periodic_position_sender, args=(interval,))
-    position_listener_thread.start()
+    cst.position_listener_thread.start()
 
     inflog("Launching comunication_node")
-    comunication_nodes_thread = threading.Thread(
+    cst.comunication_nodes_thread = threading.Thread(
         target=comunication_node, args=(interval,))
-    comunication_nodes_thread.start()
+    cst.comunication_nodes_thread.start()
 
     inflog("Launching listen_tap_devices")
-    listen_tap_devices_thread = threading.Thread(
+    cst.listen_tap_devices_thread = threading.Thread(
         target=listen_tap_devices, args=(sockets,))
-    listen_tap_devices_thread.start()
+    cst.listen_tap_devices_thread.start()
 
     listen_control_tap(control_socket)
 
 
 def stop_simulation():
     """Arrete la simulation."""
-    global stop_state
 
     inflog("Stopping all threads")
-    stop_state = True
+    cst.stop_state = True
 
-    if position_listener_thread is not None:
-        position_listener_thread.join()
+    if cst.position_listener_thread is not None:
+        cst.position_listener_thread.join()
         inflog("periodic_position_sender is stopped")
 
-    if comunication_nodes_thread is not None:
-        comunication_nodes_thread.join()
-        inflog("comunication_nodes_thread is stopped")
+    if cst.comunication_nodes_thread is not None:
+        cst.comunication_nodes_thread.join()
+        inflog("cst.comunication_nodes_thread is stopped")
 
-    if listen_tap_devices_thread is not None:
-        listen_tap_devices_thread.join()
-        inflog("listen_tap_devices_thread is stopped")
+    if cst.listen_tap_devices_thread is not None:
+        cst.listen_tap_devices_thread.join()
+        inflog("cst.listen_tap_devices_thread is stopped")
 
     # Arrete les vehicules dans ns3
-    if len(vehicles) > 0:
+    if len(cst.vehicles) > 0:
         inflog("Stoping vehicules in ns3")
         tap_sender_control(f"set_mobility {stop_vehicules()}")
 
     inflog("Simulation terminée.")
-    if number_message_sent != 0:
-        pdr = (number_message_received/number_message_sent)*100
+    if cst.number_message_sent != 0:
+        pdr = (cst.number_message_received/cst.number_message_sent)*100
     else:
         pdr = 0
 
-    inflog(f"Nombre de paquet envoyés: {number_message_sent}")
-    inflog(f"Nombre de paquet recu: {number_message_received}")
+    inflog(f"Nombre de paquet envoyés: {cst.number_message_sent}")
+    inflog(f"Nombre de paquet recu: {cst.number_message_received}")
     inflog(f"Taux de livraison des paquets: PDR = {pdr}%")
     inflog("Interruption reseau avec NS3")
     rclpy.shutdown()
@@ -206,11 +191,11 @@ def listen_control_tap(control_socket):
     """Écoute sur le tape de controle et controle les threads du programme."""
     while rclpy.ok():
         try:
-            if error_state:
+            if cst.error_state:
                 errlog("Erreur dans l'un des threads")
                 stop_simulation()
 
-            packet = control_socket.recv(MTU)
+            packet = control_socket.recv(cst.MTU)
 
             # Récupérer l'adresse IP de destination du paquet
             dest_ip = packet[30:34]
@@ -252,11 +237,9 @@ def listen_control_tap(control_socket):
 
 def listen_tap_devices(tap_sockets):
     """Écoute les tap devices."""
-    global number_message_received
-    global error_state
     while rclpy.ok():
         try:
-            if error_state or stop_state:
+            if cst.error_state or cst.stop_state:
                 inflog("Exiting listen_tap_devices")
                 sys.exit()
 
@@ -271,14 +254,14 @@ def listen_tap_devices(tap_sockets):
                     if splits[0] == device_name.replace("tap", ""):
                         # compte uniquement si l'on est la cible
 
-                        number_message_received += 1
+                        cst.number_message_received += 1
                         # A faire: traier ce qu'on recoit sur tap1,2,3,...
                         inflog(f"{device_name} received a packet from tap{splits[1]}")
                         inflog(f"Received packet (decoded): {message}")
 
         except Exception as e:
             errlog(f"Erreur lors de l'écoute des tap devices: {e}")
-            error_state = True
+            cst.error_state = True
 
 
 def periodic_position_sender(interval):
@@ -288,9 +271,8 @@ def periodic_position_sender(interval):
     Envoie les positions et vitesses de tous les véhicule.
     Demande également le temps de la simulation.
     """
-    global error_state
     while rclpy.ok():
-        if error_state or stop_state:
+        if cst.error_state or cst.stop_state:
             inflog("Exiting periodic_position_sender")
             sys.exit()
         try:
@@ -302,7 +284,7 @@ def periodic_position_sender(interval):
             errlog(
                 "Erreur lors de la récupération/"
                 + f"envoie périodique des positions : {e}")
-            error_state = True
+            cst.error_state = True
 
 
 def comunication_node(interval):
@@ -312,10 +294,8 @@ def comunication_node(interval):
     Envoie sur tous les tap la position du véhicule avec une destination
     aléatoire pour que cette information soit transmise par Wave dans NS3
     """
-    global error_state
-    global number_message_sent
     while rclpy.ok():
-        if error_state or stop_state:
+        if cst.error_state or cst.stop_state:
             inflog("Exiting comunication_node")
             sys.exit()
         try:
@@ -324,14 +304,14 @@ def comunication_node(interval):
                 dest_node = node
                 while dest_node == node:
                     dest_node = random.randint(1, cst.nb_nodes)
-                position = get_position(vehicles[node-1])
+                position = get_position(cst.vehicles[node-1])
                 tap_sender(f"{dest_node} {node} position {position}", node)
-                number_message_sent += 1
+                cst.number_message_sent += 1
             time.sleep(interval)
         except Exception as e:
             errlog("Erreur lors de la récupération/"
                    + f"envoie périodique des positions : {e}")
-            error_state = True
+            cst.error_state = True
 
 
 if __name__ == '__main__':
