25e9fc3ff68273ad44d02eeac78df3fdabb36135
SENECHAL Louis <louis.senechal@uha.fr>
2026-01-06 22:15:01 +0100

    séparation des responsabilités

--- ros2-humble/ros2_ws/src/carla_ros2_ns3/carla_ros2_ns3/lib/mock_vehicule.py
+++ ros2-humble/ros2_ws/src/carla_ros2_ns3/carla_ros2_ns3/lib/mock_vehicule.py
@@ -15,8 +15,6 @@ class Vehicule:
         self.transform = transform
         self.velocity = velocity
         self.is_alive = True
-        self.stop = True
-        self.autopilot_thread = None
 
     def get_transform(self):
         """Return a mock vehicule transform."""
@@ -26,34 +24,12 @@ class Vehicule:
         """Return a mock vehicule velocity."""
         return self.velocity
 
-    def move(self, mult=1):
-        """Move the vehicule."""
-        vx = self.velocity.x * mult
-        vy = self.velocity.y * mult
-        vz = self.velocity.z * mult
-        self.transform.move(vx, vy, vz)
-
-    def autopilot(self, interval):
-        """Thread for autopilot."""
-        while self.stop is False:
-            self.move(interval)
-            time.sleep(interval)
-        sys.exit()
-
     def destroy(self):
         """Mock vehicule destruction."""
         self.is_alive = False
 
     def set_autopilot(self, status, port):
         """Set up the autopilot."""
-        if status:
-            self.stop = False
-            self.autopilot_thread = threading.Thread(
-                target=self.autopilot, args=(cst.interval,))
-            self.autopilot_thread.start()
-        else:
-            self.stop = True
-            self.autopilot_thread.join()
 
 
 class Transform:
@@ -63,7 +39,7 @@ class Transform:
         self.location = location
 
     def move(self, vx, vy, vz):
-        """Set transform location."""
+        """Move transform location."""
         self.location.x = self.location.x + vx
         self.location.y = self.location.y + vy
         self.location.z = self.location.z + vz
@@ -102,4 +78,31 @@ class ConstantVelocityVehicule(Vehicule):
     def __init__(self, node_id, x, y, z, vx, vy, vz):
         transform = Transform(Location(x, y, z))
         velocity = Velocity(vx, vy, vz)
+        self.autopilot_thread = None
+        self.stop = True
         Vehicule.__init__(self, node_id, transform, velocity)
+
+    def set_autopilot(self, status, port):
+        """Set up the autopilot."""
+        if status:
+            self.stop = False
+            self.autopilot_thread = threading.Thread(
+                target=self.autopilot, args=(cst.interval,))
+            self.autopilot_thread.start()
+        else:
+            self.stop = True
+            self.autopilot_thread.join()
+
+    def autopilot(self, interval):
+        """Thread for autopilot."""
+        while self.stop is False:
+            self.move(interval)
+            time.sleep(interval)
+        sys.exit()
+
+    def move(self, mult=1):
+        """Move the vehicule."""
+        vx = self.velocity.x * mult
+        vy = self.velocity.y * mult
+        vz = self.velocity.z * mult
+        self.transform.move(vx, vy, vz)
